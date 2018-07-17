b1d4449ccc1a615ece7cc0dbadb3218ee8b7d858
Naveed Kakal <naveed.kakal@gmail.com>
2018-07-17 10:24:24 -0500

    Forgot the s_shape island

--- islands_engine/lib/islands_engine/island.ex
+++ islands_engine/lib/islands_engine/island.ex
@@ -47,5 +47,6 @@ defmodule IslandsEngine.Island do
   defp offsets(:atoll), do: [{0, 0}, {0, 1}, {1, 1}, {2, 0}, {2, 1}]
   defp offsets(:l_shape), do: [{0, 0}, {1, 0}, {2, 0}, {2, 1}]
   defp offsets(:dot), do: [{0, 0}]
+  defp offsets(:s_shape), do: [{0, 1}, {0, 2}, {1, 0}, {1, 1}]
   defp offsets(_), do: {:error, :invalid_island_type}
 end
